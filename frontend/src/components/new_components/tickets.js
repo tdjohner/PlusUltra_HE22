3b0a3d92a1ae72951eed143541f3ba8f25117438
Ben Warrington <ben.warrington.bh@gmail.com>
2022-01-16 06:48:16 +0000

    midnight fe update, connect to db before sleep potentially

--- frontend/src/components/new_components/tickets.js
+++ frontend/src/components/new_components/tickets.js
@@ -4,16 +4,22 @@ const Tickets = ({tickets}) => {
 
     return(
 
-        tickets.map((ticket, index) => 
+        <>
+
+        {tickets.map((ticket, index) => 
 
             <div key={index} className='ticket'>
 
-                <div><div>{ticket.title}</div><div>Grand Hall 2018</div></div>
-                <div></div> {/* ticket url image from dc db */}
+                {/* on hover show more info */}
+
+                <div><div>{ticket.title}</div><div>{(ticket.desc.length <= 50) ? ticket.desc : ticket.desc.substr(0, 50) + '...'}</div></div>
+                <div><img src={'data:image/jpeg;base64,' + btoa(String.fromCharCode.apply(null, ticket.hex.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")))}></img></div>
 
             </div>
 
-        )
+        )}<div className="ticket" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '1.25vw'}}>No tickets to load</div>
+
+        </>
 
     )
 
