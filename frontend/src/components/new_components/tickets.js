e21bc83b9b9d96f53488f4539d7ba785d9b0185f
Ben Warrington <ben.warrington.bh@gmail.com>
2022-01-16 03:58:27 +0000

    ksks

--- frontend/src/components/new_components/tickets.js
+++ frontend/src/components/new_components/tickets.js
@@ -9,8 +9,7 @@ const Tickets = ({tickets}) => {
             <div key={index} className='ticket'>
 
                 <div><div>{ticket.title}</div><div>Grand Hall 2018</div></div>
-                <div>{(ticket.desc.length <= 150) ? ticket.desc : ticket.desc.substr(0, 150) + '...' }</div>
-                <div>{ticket.url}</div>
+                <div></div> {/* ticket url image from dc db */}
 
             </div>
 
